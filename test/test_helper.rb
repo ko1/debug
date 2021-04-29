95619432306070eddbc6f575b53be498430e232a
Naoto Ono <nono19@students.desu.edu>
2021-04-29 19:18:23 +0900

    create test_case.rb and modify test_helper.rb -extend minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "debug/version"
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'debug/version'
+require_relative 'support/test_case'
 
-require "minitest/autorun"
+DEBUGGER__::TestCase.before_suite
